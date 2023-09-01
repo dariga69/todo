5ec7230a069ea082ff98fd232eec7ba82a88bce7
dariga69 <dariga6996@gmail.com>
2023-09-01 17:32:05 +0600

    name

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,47 @@
 from django.shortcuts import render
 from tasks.models import Task
-
+from django.shortcuts import redirect
 
 def get_list(request):
     if request.method == 'GET':
         tasks = Task.objects.all()
         return render(request, 'index.html', {"tasks": tasks})
+
+def create_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        created_at = request.POST.get('created_at')
+        type = request.POST.get('type')
+        task = Task.objects.create(title=title, description=description, created_at=created_at, type=type)
+        return redirect('index')
+    return render(request, 'index.html')
+
+
+def update_task(request, id):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        created_at = request.POST.get('created_at')
+        type = request.POST.get('type')
+        task = Task.objects.get(id=id)
+        task.title = title
+        task.description = description
+        task.created_at = created_at
+        task.type = type
+        task.save()
+        return redirect('index')
+    context = {
+        'data': Task.objects.get(id=id)
+    }
+    return render(request, 'update.html', context)
+
+
+def delete_task(request, id):
+    if request.method == 'POST':
+        task = Task.objects.get(id=id)
+        task.delete()
+        return redirect('index')
+    return render(request, 'index.html')
+
+
